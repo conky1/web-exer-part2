6429289129c949d69d4dfe3b35a5c21f8f21d419
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-14 16:45:42 +0300

    j

--- phonebook/src/components/Form.js
+++ phonebook/src/components/Form.js
@@ -15,14 +15,14 @@ const Form = ({ formSubmit, newName, newphone, setNewName, setNewPhone }) => {
 
         <form onSubmit={formSubmit} >
             <div>
-                <h2>Add a new</h2>
+                <h2>Add new person</h2>
   Name: <input value={newName}
                     onChange={handleNameChange} /> <br></br>
     Number: <input value={newphone}
                     onChange={handlePhoneChange} />
             </div>
             <div>
-                <button type="submit">add</button>
+                <button type="submit">Add</button>
             </div>
         </form>
 
