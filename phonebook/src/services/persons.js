d43ab00b29b1168c0edae6b887f67588ceb21134
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-15 10:41:31 +0300

    j

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -3,29 +3,29 @@ import axios from 'axios'
 
 const baseUrl = '/api/persons'
 
-const getAll = () => {
+const getAll = async () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  const response = await request
+  return response.data
 }
 
-const create = newObject => {
+const create = async newObject => {
   const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+  const response = await request
+  return response.data
 }
 
-const update = (id, newObject) => {
+const update = async (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+  const response = await request
+  return response.data
 }
 
-const remove = (id) => {
+const remove = async (id) => {
   const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(response => response.data)
+  const response = await request
+  return response.data
 }
 
-const exportedObject = {
-  getAll,
-  create,
-};
 
-export default { getAll, create, update, remove, exportedObject }
+export default  {getAll, create, update, remove} 
